6ee2a496c155223929466892392ebb35ff7f755f
cse0518 <60170616+cse0518@users.noreply.github.com>
2020-11-19 00:47:24 +0900

    mission complete
    good

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import random
 
 app = Flask(__name__)
 
-
 lotto_numbers = list(range(1,46))
 
 @app.route("/")
 def hello():
-
     return render_template("index.html", variable=lotto)
 
 @app.route("/lotto",methods=['GET'])
@@ -17,10 +15,32 @@ def lotto():
     return jsonify(lotto)
 
 def select_numbers(select_amount:int):
-    ### 이곳을 같이 채워봐요
+    ## 01
+    lotto = []
+    for i in range(select_amount):
+        while True:
+            selected_num = random.randint(1,45)
 
-    lotto = [1,2,3,4,5,6]
+            is_duplicated = False
+            for num in lotto:
+                if selected_num == num:
+                    is_duplicated = True
+                    break
 
+            if is_duplicated:
+                continue
+            lotto.append(selected_num)
+            break
+
+    ## 02
+    #candidate_numbers = lotto_numbers[:]
+    #lotto = []
+    #for i in range(select_amount):
+    #    selected_num = candidate_numbers.pop(random.randrange(len(candidate_numbers)))
+    #    lotto.append(selected_num)
+
+    ## 03
+    #lotto = random.sample(lotto_numbers, select_amount)
 
     return lotto
 
